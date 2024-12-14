4a04d7dfc9973f76143e2e5f65ece97b6d10d52d
Mahesh8188 <ddbambhava8182@gmail.com>
2024-12-14 15:17:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,11 +9,11 @@ class Config:
     API_ID = environ.get("API_ID", "26741021")
     API_HASH = environ.get("API_HASH", "7c5af0b88c33d2f5cce8df5d82eb2a94")
     BOT_TOKEN = environ.get("BOT_TOKEN", "7702869138:AAHOPg17Eh2pUPtUWXFgkeBumY8SHFzo2rw") 
-    BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
+    BOT_SESSION = environ.get("BOT_SESSION", "Forward6336bot") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://mahesh12:mahesh12@cluster0.hscxg.mongodb.net/?retryWrites=true&w=majority")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6859451629').split()]
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 'https://t.me/+BEDYUyV4D3xhMmJl'))
+    DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster 0")
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6471106079 6859451629').split()]
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002399867259'))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/vegamoviesnewin") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
     PORT = environ.get('PORT', '8080')
