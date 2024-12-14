232d494c3d8078aad811793bc294ac0ccc817bd4
Mahesh8188 <ddbambhava8182@gmail.com>
2024-12-14 15:17:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://mahesh12:mahesh12@cluster0.hscxg.mongodb.net/?retryWrites=true&w=majority")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster 0")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6471106079 6859451629').split()]
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002399867259'))
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002084819782'))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/vegamoviesnewin") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
     PORT = environ.get('PORT', '8080')
