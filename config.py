8d2e8b70d9979ff333c200cb287bcf2ca5603e12
Mahesh8188 <ddbambhava8182@gmail.com>
2025-01-19 13:10:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ from os import environ
 class Config:
     API_ID = environ.get("API_ID", "26741021")
     API_HASH = environ.get("API_HASH", "7c5af0b88c33d2f5cce8df5d82eb2a94")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7702869138:AAHOPg17Eh2pUPtUWXFgkeBumY8SHFzo2rw") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "vegamoviesforwordbot") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://mahesh81:mahesh81@cluster0.ozx17.mongodb.net/?retryWrites=true&w=majority")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
